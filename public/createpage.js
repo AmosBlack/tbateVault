b2281e7a683d210190d32ff127df8beea763fbd3
AmosBlack <ojasmittal2008@gmail.com>
2023-06-26 16:29:54 +0530

    db chapter view

--- public/createpage.js
+++ public/createpage.js
@@ -1,50 +1,72 @@
-let chaptersCollection = document.getElementsByClassName("chap-li")
-console.log(chaptersCollection)
-function createChapterModal(chapNumber) {
-  var chapterText;
 
+//chapters-list in html
+let chaptersCollection = document.getElementsByClassName("chap-li")
+
+//when chap-li clicked, create modal
+function createChapterModal(chapNumber) {
+  var chapterText; //stores chapter text (global)
+
+  //pull data from firebase asynchronously
   firebase
     .database()
     .ref(`chapters/${chapNumber}`)
     .once("value")
     .then((snapshot) => {
       chapterText = snapshot.val();
-
+      //create modal
       var chapterModal = document.createElement("dialog");
-
+      chapterModal.classList.add("chapter-modal")
+      //create title
       var chapterTitle = document.createElement("h1");
       chapterTitle.textContent = `Chapter ${chapNumber}`;
       chapterModal.appendChild(chapterTitle);
-
-      var chapterContent = document.createElement("div");
+      //paragraph el containing text
+      var chapterContent = document.createElement("p");
       chapterContent.innerHTML = chapterText;
       chapterModal.appendChild(chapterContent);
-
+      //close modal
       var chapterClose = document.createElement("button");
-      chapterClose.textContent = "Close";
+      chapterClose.classList.add("button")
+      chapterClose.classList.add("cross")
+      chapterClose.textContent = "X";
+      //on-click func
       chapterClose.addEventListener("click", function () {
         chapterModal.close();
+        chapterModal.remove()
       });
       chapterModal.appendChild(chapterClose);
-
+      //go to previous chapter
       var chapterPrevious = document.createElement("button");
+      chapterPrevious.classList.add("button")
+      chapterPrevious.classList.add("previous")
       chapterPrevious.textContent = "Previous";
+      //hardcoded length- to fix
+      if(chapNumber <= 1){
+        chapterPrevious.style.display = "none"
+      }
       chapterPrevious.addEventListener("click", function () {
-        // Logic for going to the previous chapter
-        // You can implement the desired functionality here
-        console.log("Previous button clicked");
+          createChapterModal(chapNumber-1)
+          chapterModal.remove()
       });
       chapterModal.appendChild(chapterPrevious);
-
+      //next chapter button
       var chapterNext = document.createElement("button");
+      chapterNext.classList.add("button")
+      chapterNext.classList.add("next")
       chapterNext.textContent = "Next";
+      //current hardcoded length- to fix
+      if(chapNumber>=7){
+        chapterNext.style.display = "None"
+      }
+      //go to next chapter
       chapterNext.addEventListener("click", function () {
-        // Logic for going to the next chapter
-        // You can implement the desired functionality here
-        console.log("Next button clicked");
+        createChapterModal(chapNumber+1)
+        chapterModal.remove()
       });
       chapterModal.appendChild(chapterNext);
-
+      //append modal to body
+      document.body.appendChild(chapterModal)
+      //open modal
       chapterModal.showModal();
     })
     .catch((error) => {
@@ -52,8 +74,12 @@ function createChapterModal(chapNumber) {
     });
 }
 
+//for each chap-li, on-click create chapter modal
 Array.from(chaptersCollection).forEach((element, index) => {
   element.addEventListener("click", () => {
-    createChapterModal(index);
+    var chapterNumber = index+1
+    createChapterModal(chapterNumber);
   });
 });
+
+
